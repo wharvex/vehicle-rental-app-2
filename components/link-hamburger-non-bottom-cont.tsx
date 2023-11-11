15e9e64c2a4cd5f2ef8ed6e084d5678ad37782a3
Tara Pedigo <tkirkhus@gmail.com>
2023-11-11 16:54:14 -0500

    Add link page parameter

--- components/link-hamburger-non-bottom-cont.tsx
+++ components/link-hamburger-non-bottom-cont.tsx
@@ -1,14 +1,25 @@
 import type { NextPage } from "next";
+import { useCallback } from "react";
+import { useRouter } from "next/router";
 
 type LinkHamburgerNonBottomContType = {
   linkText?: string;
+  linkPage?: string;
 };
 
+
 const LinkHamburgerNonBottomCont: NextPage<LinkHamburgerNonBottomContType> = ({
-  linkText = "Link / Hamburger / Non-Bottom",
+  linkText = "Link / Hamburger / Non-Bottom", linkPage = "link page"
 }) => {
+
+  const router = useRouter();
+  const onLinkClick = useCallback(() => {
+    router.push(linkPage);
+  }, [router]);
+  
   return (
-    <div className="box-border flex flex-row items-center justify-center py-[50px] px-0 text-center text-21xl text-black font-reg-medium self-stretch border-b-[1px] border-solid border-black">
+    <div className="box-border flex flex-row items-center justify-center py-[50px] px-0 text-center text-21xl text-black font-reg-medium self-stretch border-b-[1px] border-solid border-black cursor-pointer"
+      onClick={onLinkClick}>
       <a className="[text-decoration:none] relative tracking-[0.5px] leading-[100%] font-medium text-[inherit] flex items-center justify-center w-[340px] shrink-0">
         {linkText}
       </a>
